499aa9c07502e2a44abe92de5ab54b98316a42e0
Skalin <DominikSkala@seznam.cz>
2017-12-04 02:08:20 +0100

    - renaming of some variables to english (code consistency)

--- main.cpp
+++ main.cpp
@@ -8,30 +8,28 @@
 
 
 
-const double RouteA = 3600.0; // doba cesty na trase Veseli - Bucovice
-const double RouteB = 10.0*60.0; // doba cesty na trase Bucovice - Slavkov
-const double RouteC = 25.0*60.0; // doba cesty na trase Slavkov - Brno
+const double routes[3] = {60.0*60.0, 10.0*60.0, 25.0*60.0}; // doba cesty na jednotlivych usecich - Veseli - Bucovice, Bucovice - Slavkov, Slavkov - Brno
 const double PeakTime = 180.5; // doba trvani spicky v minutach
 const double NonPeakTime = 780.5; // doba trvani nespicky v minutach(cas mezi spickou a noci)
 
-const double SpickaIntervalVlaku = 60; // interval mezi vlaky ve spicce
-const double NespickaIntervalVlaku = 120; // interval mezi vlaky mimo spickuS
-const int dobaNastupu = 2;
-const int pocetVagonu = 3;
+const double PeakTimeInterval = 60; // interval mezi vlaky ve spicce
+const double NonPeakTimeInterval = 120; // interval mezi vlaky mimo spickuS
+const int timeToEnter = 2;
+const int amountOfWagons = 3;
 const double pocetMistVeVagonu = 33.3;
-const int pocetVstupu = pocetVagonu*2;
+const int amountOfEntersIntoWagon = amountOfWagons*2;
 
-const double pocetLidiVeSpicce = 0.54; // procent lidi ve spicce
-const double VlakyVeSpicce = PeakTime/SpickaIntervalVlaku;
-const double VlakyMimoSpicku = NonPeakTime/NespickaIntervalVlaku;
+const double PeakTimeParameter = 0.54; // procent lidi ve spicce
+const double TrainsInPeakTime = PeakTime/PeakTimeInterval;
+const double TrainsInNonPeakTime = NonPeakTime/NonPeakTimeInterval;
 
-const int cestujici[3] = {550, 250, 200};
+const int passengers[3] = {550, 250, 200};
 
-int arrivals[] = {5*3600, 6*3600, 7*3600, 8*3600, 10*3600, 12*3600, 14*3600, 16*3600, 18*3600, 20*3600, 22*3600};
+int departures[] = {4*3600+3000, 6*3600+60, 7*3600+60, 9*3600+60, 11*3600+60, 13*3600+60, 15*3600+60, 17*3600+60, 19*3600+60, 21*3600+60};
 
 
 const int amountOfStations = 4;
-Queue cekarna[3];
+Queue waitingRooms[3];
 
 Facility Stations[amountOfStations];
 
@@ -44,7 +42,7 @@ int TimeOfDay(){
 
 
 
-int castDne(int time) {
+int partOfDay(int time) {
 	if ((time >= 4*3600+1800) && (time <= 8*3600+1800)) {
 		return 1;
 	} else if ((time > (8*3600+1800)) && (time <= 22*3600)) {
@@ -55,53 +53,65 @@ int castDne(int time) {
 }
 
 
-class Vlak : public Process {
+class Train : public Process {
 public:
-	Vlak(int time) : Process() {
+	Train(int time) : Process() {
 		initDepartureTime = time;
-		store = new Store((unsigned int) (pocetVagonu*pocetMistVeVagonu));
+		store = new Store((unsigned int) round(amountOfWagons*pocetMistVeVagonu));
 		currentTime = initDepartureTime;
 	}
 
 
 	void Behavior() {
 
+		currentStation = -1;
 		this->currentTime = TimeOfDay();
 		Seize(Stations[0]);
-		Wait(20); // zastaveni vlaku
-		Print("Waitin..\n");
+		currentStation = 0;
+		//Wait(20); // zastaveni vlaku
+		Wait(2);
 
-		//Wait(((this->store->Free() > cekarna[0].Length()) ? (cekarna[0].Length()*pocetVagonu/pocetVstupu) : (this->store->Free())*pocetVagonu/pocetVstupu));
+		//Wait(((this->store->Free() > waitingRooms[0].Length()) ? (waitingRooms[0].Length()*amountOfWagons/pocetVstupu) : (this->store->Free())*amountOfWagons/pocetVstupu));
 		Release(Stations[0]);
-		//Print("---------------------------------------\n");
-		//Print("Vlak v %d jede do Bucovic, vyjizdi v %f\n", cas/3600, Time/3600);
-		Wait(RouteA);
-		//Print("Vlak v %d | Trvani: %.0f (min) | Dojel v %.5f (hod) \n", cas/3600, RouteA/60.0, (Time)/3600);
+		currentStation = -1;
+		Print("---------------------------------------\n");
+		Print("Vlak v %d jede do Bucovic, vyjizdi v %f\n", getCurrentTime()/3600, Time/3600);
+		Wait(routes[0]);
+		Print("Vlak v %d | Trvani: %.0f (min) | Dojel v %.5f (hod) \n", getCurrentTime()/3600, routes[0]/60.0, (Time)/3600);
 
 		//Print("---------------------------------------\n");
 		this->currentTime = TimeOfDay();
+		std::cout<< "Aktualni cas: " << this->getCurrentTime()/3600 << std::endl;
 		Seize(Stations[1]);
-		Wait(20); // zastaveni vlaku
-		//Wait((this->store->Free() > cekarna[1].Length() ? cekarna[1].Length() : this->store->Free())*pocetVagonu/pocetVstupu);
+		currentStation = 1;
+		Wait(2);
+		//Wait(20); // zastaveni vlaku
+		//Wait((this->store->Free() > waitingRooms[1].Length() ? waitingRooms[1].Length() : this->store->Free())*pocetVagonu/pocetVstupu);
 		Release(Stations[1]);
-		//Print("---------------------------------------\n");
-		//Print("Vlak v %d jede do Slavkova\n", cas/3600);
-		Wait(RouteB);
-//		Print("Vlak v %d | Trvani: %.0f (min) | Dojel v %.2f (hod)\n", cas/3600, RouteB/60.0, (Time)/3600);
-		//Print("---------------------------------------\n");
+		currentStation = -1;
+		Print("---------------------------------------\n");
+		Print("Vlak v %d jede do Slavkova\n", getCurrentTime()/3600);
+		Wait(routes[1]);
+		Print("Vlak v %d | Trvani: %.0f (min) | Dojel v %.2f (hod)\n", getCurrentTime()/3600, routes[1]/60.0, (Time)/3600);
+		Print("---------------------------------------\n");
 		this->currentTime = TimeOfDay();
+		std::cout<< "Aktualni cas: " << this->getCurrentTime()/3600 << std::endl;
 		Seize(Stations[2]);
-		Wait(20); // zastaveni vlaku
-		//Wait((this->store->Free() > cekarna[2].Length() ? cekarna[2].Length() : this->store->Free())*pocetVagonu/pocetVstupu);
+		currentStation = 2;
+		//Wait(20); // zastaveni vlaku
+		//Wait((this->store->Free() > waitingRooms[2].Length() ? waitingRooms[2].Length() : this->store->Free())*pocetVagonu/pocetVstupu);
 		Release(Stations[2]);
-		//Print("---------------------------------------\n");
-		//Print("Vlak v %d jede do Brna\n", cas/3600);
-		Wait(RouteC);
-		//Print("Vlak v %d | Trvani: %.0f (min)| Dojel v %.2f (hod)\n", cas/3600, RouteC/60.0, (Time)/3600);
+		currentStation = -1;
+		Print("---------------------------------------\n");
+		Print("Vlak v %d jede do Brna\n", getCurrentTime()/3600);
+		Wait(routes[2]);
+		Print("Vlak v %d | Trvani: %.0f (min)| Dojel v %.2f (hod)\n", getCurrentTime()/3600, routes[2]/60.0, (Time)/3600);
 
 		this->currentTime = TimeOfDay();
+		std::cout<< "Aktualni cas: " << this->getCurrentTime()/3600 << std::endl;
 		Seize(Stations[3]);
-		Wait(20); // zastaveni vlaku
+		currentStation = 3;
+		//Wait(20); // zastaveni vlaku
 
 		Release(Stations[3]);
 	}
@@ -110,7 +120,11 @@ public:
 		return initDepartureTime;
 	}
 
-	double getCurrentTime() const {
+	int getCurrentStation() const {
+		return currentStation;
+	}
+
+	int getCurrentTime() const {
 		return currentTime;
 	}
 
@@ -123,20 +137,21 @@ public:
 private:
 	int initDepartureTime;
 	Store *store;
-	double currentTime;
+	int currentTime;
+	int currentStation;
 };
 
 
-std::vector<Vlak*> vlaky;
+std::vector<Train*> trains;
 
 
-int nejblizsiVlak(int time) {
+int nejblizsiVlak(int time, int station) {
 	int nearest = 0;
-	for (unsigned int i = 0; i < vlaky.size(); i++) {
-		std::cout << "I: " << i << std::endl;
+	for (unsigned int i = 0; i < trains.size(); i++) {
+		/*std::cout << "I: " << i << std::endl;
 		std::cout << "Time: " << Time << std::endl;
-		std::cout << "Aktualni cas vlaku: " << vlaky.at(i)->getCurrentTime()/3600 << std::endl;
-		if ((vlaky.at(i)->getCurrentTime() - time) > (vlaky.at(nearest)->getCurrentTime() - time)) {
+		std::cout << "Aktualni cas vlaku: " << trains.at(i)->getCurrentTime()/3600 << std::endl;*/
+		if ((trains.at(i)->getCurrentTime() - time) > (trains.at(nearest)->getCurrentTime() - time)) {
 			nearest = i;
 		}
 	}
@@ -144,10 +159,10 @@ int nejblizsiVlak(int time) {
 }
 
 
-class Cestujici : public Process {
+class Passenger : public Process {
 public:
-	Cestujici(int Stanice) : Process() {
-		stanice = Stanice;
+	Passenger(int Station) : Process() {
+		station = Station;
 	}
 
 
@@ -157,97 +172,86 @@ public:
 
 
 
-void Behavior() {
-		double Prichod;
-		Prichod = Time;
+	void Behavior() {
+		double Came;
+		Came = Time;
 		inTrain = false;
 
 
-		Into(cekarna[stanice]);
+		Into(waitingRooms[station]);
 	enterTrain:
-		unsigned long nearest = (unsigned long) nejblizsiVlak(TimeOfDay());
-		if (Stations[stanice].Busy() && !vlaky.at(nearest)->isFull()) {
-			cekarna[stanice].GetFirst();
-			Enter(*vlaky.at(nearest)->getStore());
+		unsigned long nearest = (unsigned long) nejblizsiVlak(TimeOfDay(), station);
+		if (Stations[station].Busy() && !trains.at(nearest)->isFull()) {
+			waitingRooms[station].GetFirst();
+			Enter(*trains.at(nearest)->getStore());
 			inTrain = true;
 		} else {
-			WaitUntil(Stations[stanice].Busy());
+			Wait(2);
 			goto enterTrain;
 		}
-/*
-		if (!Stanice[stanice].Busy() || !vlaky.at(nearest)->isFull()) {
-			Into(cekarna[stanice]);
-			WaitUntil(Stanice[stanice].Busy() && !vlaky.at(nearest)->isFull());
-			cekarna[stanice].GetFirst();
-			goto enterTrain;
-		}
-		if (Stanice[stanice].Busy() && !isInTrain() && !vlaky.at(nearest)->isFull()) {
-			Enter(*vlaky.at(nearest)->getStore());
-			inTrain = true;
-			Table(Time-Prichod);
-		} else {
-
-		}
-*/
 
 
-		for (int i = stanice+1; i < amountOfStations; i++) {
-			WaitUntil(inTrain && Stations[i].Busy());
+		for (int i = station+1; i < amountOfStations; i++) {
+
 			double vystup = Random();
-			if (isInTrain() && vystup <= 0.1) {
-				Leave(*vlaky.at(nearest)->getStore());
+			test:
+			if (Stations[i].Busy() && isInTrain() && vystup <= 0.1) {
+				Leave(*trains.at(nearest)->getStore());
 				inTrain = false;
+			} else {
+				Wait(2);
+				goto test;
 			}
 		}
 
 
-		if (Stations[2].Busy() && inTrain) {
-			Leave(*vlaky.at(nearest)->getStore());
+		if (Stations[2].Busy() && isInTrain()) {
+			Leave(*trains.at(nearest)->getStore());
 		}
 
 	}
 
 	bool inTrain;
-	int stanice;
+	int station;
 };
 
 
 
-class GeneratorCestujici : public Event {
+class PassengerGenerator : public Event {
 public:
-	GeneratorCestujici(int Stanice) : Event() {
-		stanice = Stanice; // stanice 0 je Veseli, stanice 1 je Bucovice, 2 je Slavkov (negenerujeme nic)
+	PassengerGenerator(int Station) : Event() {
+		station = Station; // stanice 0 je Veseli, stanice 1 je Bucovice, 2 je Slavkov (negenerujeme nic)
 	}
 
 	void Behavior() {
 		int time = TimeOfDay();
-		if (castDne(time) == 1) {
-			Activate(Time+Exponential(SpickaIntervalVlaku/((cestujici[stanice]*pocetLidiVeSpicce)/PeakTime)));
-		} else if (castDne(time) == 2) {
-			Activate(Time+Exponential(SpickaIntervalVlaku/((cestujici[stanice]*(1.00-pocetLidiVeSpicce))/NonPeakTime)));
+		if (partOfDay(time) == 1) {
+			Activate(Time+Exponential(PeakTimeInterval/((passengers[station]*PeakTimeParameter)/PeakTime)));
+		} else if (partOfDay(time) == 2) {
+			Activate(Time+Exponential(NonPeakTimeInterval/((passengers[station]*(1.00-PeakTimeParameter))/NonPeakTime)));
 		} else {
 			Activate(Time+Normal(3600, 55));
 		}
 
-		(new Cestujici(stanice))->Activate();
+		(new Passenger(station))->Activate();
 	}
 
-	int stanice;
+	int station;
 
 };
 
 
 
-class GeneratorVlak : public Process {
+class TrainGenerator : public Process {
 	void Behavior() {
-		Vlak* vlak;
-		int size = sizeof(arrivals)/sizeof(arrivals[0]);
+		Train* vlak;
+		int size = sizeof(departures)/sizeof(departures[0]);
 		for (int i = 0; i < size; i++) {
 		prijezd:
 			int time = TimeOfDay();
-			if (time == arrivals[i]) {
-				vlak = (new Vlak(arrivals[i]));
-				vlaky.push_back(vlak);
+			if (time == departures[i]) {
+				vlak = (new Train(departures[i]));
+				trains.push_back(vlak);
 				vlak->Activate(Time);
 			} else {
 				Wait(1);
@@ -263,10 +267,10 @@ int main() {
 	RandomSeed(time(NULL));
 	Init(0, 86400);
 
-	(new GeneratorCestujici(0))->Activate();
-	(new GeneratorCestujici(1))->Activate();
-	(new GeneratorCestujici(2))->Activate();
-	(new GeneratorVlak())->Activate();
+	(new PassengerGenerator(0))->Activate();
+	(new PassengerGenerator(1))->Activate();
+	(new PassengerGenerator(2))->Activate();
+	(new TrainGenerator())->Activate();
 	Run();
 	//Table.Output();
 	//Trains.Output();
@@ -274,11 +278,11 @@ int main() {
 	Stations[1].Output();
 	Stations[2].Output();
 	Stations[3].Output();
-	cekarna[0].Output();
-	cekarna[1].Output();
-	cekarna[2].Output();
-	for (unsigned int i = 0; i < vlaky.size(); i++) {
-		vlaky.at(i)->getStore()->Output();
+	waitingRooms[0].Output();
+	waitingRooms[1].Output();
+	waitingRooms[2].Output();
+	for (unsigned int i = 0; i < trains.size(); i++) {
+		trains.at(i)->getStore()->Output();
 	}
 
 	return 0;
